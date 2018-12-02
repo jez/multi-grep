027081f919707d742a26ac542507a2c496b467d6
Jake Zimmerman <zimmerman.jake@gmail.com>
2018-12-01 23:02:18 -0800

    Rewrite using failure create

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate regex;
+extern crate failure;
 
 use regex::Regex;
+
+use failure::Error;
 
 use std::cmp::Ordering::{Less, Equal, Greater};
 use std::env;
@@ -9,46 +12,22 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::process;
 
-fn usage(arg0: &str) -> Result<(), i32> {
-    eprintln!("usage: {} <locs.txt> <pattern>", arg0);
-    Err(1)
-}
-
-fn run() -> Result<(), i32> {
+fn run() -> Result<i32, Error> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
-        return usage(&args[0])
+        eprintln!("usage: {} <locs.txt> <pattern>", args[0]);
+        return Ok(1)
     }
 
     let input_filename = &args[1];
     let pattern = &args[2];
 
-    let re = match Regex::new(pattern) {
-        Ok(re) => re,
-        Err(error) => {
-            eprintln!("error: Invalid regex ({})", error);
-            return Err(1)
-        },
-    };
+    let re = Regex::new(pattern)?;
 
-    let input_file = match File::open(input_filename) {
-        Ok(file) => file,
-        Err(err) => {
-            eprintln!("error: Couldn't open input file. Original error:");
-            eprintln!("{}", err);
-            return Err(1)
-        }
-    };
+    let input_file = File::open(input_filename)?;
     for (i, maybe_input_line) in BufReader::new(input_file).lines().enumerate() {
-        let input_line = match maybe_input_line {
-            Ok(input_line) => input_line,
-            Err(err) => {
-                eprintln!("error: Couldn't read input line. Original error:");
-                eprintln!("{}", err);
-                return Err(1)
-            }
-        };
+        let input_line = maybe_input_line?;
 
         let parts: Vec<_> = input_line.split(":").collect();
         let (filename, lineno) = match parts.len() {
@@ -59,24 +38,17 @@ fn run() -> Result<(), i32> {
                     Ok(lineno) => (filename, lineno),
                     Err(_) => {
                         eprintln!("error: Invalid line number ({}) on line {}", lineno_str, i + 1);
-                        return Err(1)
+                        return Ok(1)
                     }
                 }
             }
             _ => {
                 eprintln!("error: Couldn't parse input line {}", i);
-                return Err(1)
+                return Ok(1)
             }
         };
 
-        let file = match File::open(filename) {
-            Ok(file) => file,
-            Err(err) => {
-                eprintln!("error: Couldn't open {}. Original error:", filename);
-                eprintln!("{}", err);
-                return Err(1)
-            }
-        };
+        let file = File::open(filename)?;
         for (j, maybe_line) in BufReader::new(file).lines().enumerate() {
             match j.cmp(&(lineno - 1)) {
                 Less => continue,
@@ -84,14 +56,7 @@ fn run() -> Result<(), i32> {
                 Equal => (),
             }
 
-            let line = match maybe_line {
-                Ok(line) => line,
-                Err(err) => {
-                    eprintln!("error: Couldn't read line {} in {}. Original error:", j, filename);
-                    eprintln!("{}", err);
-                    return Err(1)
-                }
-            };
+            let line = maybe_line?;
 
             if re.is_match(&line) {
                 println!("{}:{}", filename, lineno);
@@ -99,12 +64,12 @@ fn run() -> Result<(), i32> {
         }
     }
 
-    Ok(())
+    Ok(0)
 }
 
 fn main() {
     process::exit(match run() {
-        Ok(()) => 0,
-        Err(err) => err
+        Ok(exit_code) => exit_code,
+        Err(_err) => 1,
     });
 }
